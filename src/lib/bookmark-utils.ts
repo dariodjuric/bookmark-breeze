b019008f9237b9972129ca1a9ac0af77efd644f0
Dario Djuric <dario.djuric@gmail.com>
2025-12-21 18:55:45 +0100

    Use higher quality icon

--- src/lib/bookmark-utils.ts
+++ src/lib/bookmark-utils.ts
@@ -20,7 +20,7 @@ export function getDepthPadding(depth: number): string {
 export function getFaviconUrl(url: string): string {
   try {
     const hostname = new URL(url).hostname
-    return `https://www.google.com/s2/favicons?domain=${hostname}&sz=16`
+    return `https://www.google.com/s2/favicons?domain=${hostname}&sz=64`
   } catch {
     return ''
   }
